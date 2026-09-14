f6edd61abf2c168ae71d90e27b1b1f29c742a0b9
Minhazul Islam Rikat <minhazulrikat@gmail.com>
2026-09-14 17:23:23 +0600

    footer links are updated

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -16,24 +16,25 @@ const Footer = () => {
           </p>
         </aside>
         <nav>
-          <h6 className="text-lg text-base-200 md:mb-4">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <h6 className="text-lg text-base-200 md:mb-4">Product</h6>
+          <a className="link link-hover">Features</a>
+          <a className="link link-hover">Pricing</a>
+          <a className="link link-hover">Templates</a>
+          <a className="link link-hover">Integrations</a>
         </nav>
         <nav>
           <h6 className="text-lg text-base-200 md:mb-4">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">About </a>
+          <a className="link link-hover">Blog</a>
+          <a className="link link-hover">Careers</a>
+          <a className="link link-hover">Press</a>
         </nav>
         <nav>
-          <h6 className="text-lg text-base-200 md:mb-4">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <h6 className="text-lg text-base-200 md:mb-4">Resources</h6>
+          <a className="link link-hover">Documentation</a>
+          <a className="link link-hover">Help Center</a>
+          <a className="link link-hover"> Community </a>
+          <a className="link link-hover"> Contact </a>
         </nav>
         <nav>
           <h6 className="text-lg text-base-200 md:mb-4">Social Links</h6>
